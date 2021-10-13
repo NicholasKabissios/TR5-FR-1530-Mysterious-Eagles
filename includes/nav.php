76c2bcb330ef44e8113d92dcbfacea9a6fd0d0b9
sibelrmit <68837011+sibelrmit@users.noreply.github.com>
2021-10-13 15:37:23 +1100

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -2,8 +2,7 @@
 		<div>
 			<h1><a href="index.php" class="active">Home</a></h1>
 			<h1><a href="map.html">Map</a></h1>
-			<h1><a href="#">New Post</a></h1>
-			<h1><a href="account.html">Account</a></h1>
+			<h1><a href="account.php">Account</a></h1>
 			<h1><a href="login.php">Log In</a></h1>
 			<h1><a href="signup.php">Sign Up</a></h1>
 		</div>
