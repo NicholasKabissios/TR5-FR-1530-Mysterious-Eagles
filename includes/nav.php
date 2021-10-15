bbcdc8c746ef3f0a6becc156aec27a4542fde95a
sibelrmit <68837011+sibelrmit@users.noreply.github.com>
2021-10-15 13:16:14 +1100

    Add files via upload

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <nav class="navtop">
 		<div>
 			<h1><a href="index.php" class="active">Home</a></h1>
-			<h1><a href="map.html">Map</a></h1>
+			<h1><a href="map.php">Map</a></h1>
 			<h1><a href="account.php">Account</a></h1>
 			<h1><a href="login.php">Log In</a></h1>
 			<h1><a href="signup.php">Sign Up</a></h1>
