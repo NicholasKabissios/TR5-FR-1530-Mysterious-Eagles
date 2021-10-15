55e3e9078d4fc26a2b108e6bf909ea8d172001a5
sibelrmit <68837011+sibelrmit@users.noreply.github.com>
2021-10-15 13:15:49 +1100

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav class="navtop">
+		<div>
+			<h1><a href="index.php" class="active">Home</a></h1>
+			<h1><a href="map.php">Map</a></h1>
+			<h1><a href="account.php">Account</a></h1>
+			<h1><a href="login.php">Log In</a></h1>
+			<h1><a href="signup.php">Sign Up</a></h1>
+		</div>
+	</nav>
